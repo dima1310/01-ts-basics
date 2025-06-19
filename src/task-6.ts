d4d9301c522e82d8f36902c47486b53abb88987c
dima1310 <dimaponomarenko13101981@gmail.com>
2025-06-20 00:04:37 +0300

    fix

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,6 +2,13 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
+const firstNum = getFirstElement<number>([10, 20, 30]); // number
+console.log("First number:", firstNum);
+const firstStr = getFirstElement<string>(["a", "b", "c"]); // string
+console.log("First string:", firstStr);
+const firstBool = getFirstElement<boolean>([true, false]); // boolean
+console.log("First boolean:", firstBool);
+
 getFirstElement([1, 2, 3]); // 1
 getFirstElement(["a", "b", "c"]); // "a"
 getFirstElement([true, false, true]); // true
